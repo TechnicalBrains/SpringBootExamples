4c7f8a588acf545acceb06e934ccfaf3a27aa9a2
Ramaraju <611163@cognizant.com>
2018-08-17 15:26:36 +0530

    initial project version

---
+++ EduMgmtSys/src/main/java/com/edu/mgmt/sys/EduMgmtSysApplication.java
@@ -0,0 +1,12 @@
+package com.edu.mgmt.sys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EduMgmtSysApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EduMgmtSysApplication.class, args);
+	}
+}
